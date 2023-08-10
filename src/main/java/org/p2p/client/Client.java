466618feddd4d13dc62492b39ef72809c5104af4
starkda <d.vakhidov@innopolis.university>
2023-08-10 17:35:57 +0300

    hotfix

--- src/main/java/org/p2p/client/Client.java
+++ src/main/java/org/p2p/client/Client.java
@@ -9,14 +9,14 @@ public class Client {
 
     public static void main(String... args) {
         ManagedChannel managedChannel = ManagedChannelBuilder
-                .forTarget("0.0.0.0:9998")
+                .forTarget("0.0.0.0:9999")
                 .usePlaintext()
                 .build();
         ClientToServerCallerGrpc.ClientToServerCallerBlockingStub stub =
                 ClientToServerCallerGrpc
                 .newBlockingStub(managedChannel);
-      //  ClientToServer.RegistrationResponse registrationResponse = stub.register(ClientToServer.RegistrationRequest.newBuilder().setAddress("llr").build());
-     //  System.out.println(registrationResponse.getUserId());
+        ClientToServer.RegistrationResponse registrationResponse = stub.register(ClientToServer.RegistrationRequest.newBuilder().setAddress("llr").build());
+       System.out.println(registrationResponse.getUserId());
       //  ClientToServer.ClientAddressResponse clientAddressResponse = stub.getClientAddress(ClientToServer.ClientAddressRequest.newBuilder().setClientId(String.valueOf(999999)).setReceiverId(String.valueOf(2)).build());
       //  System.out.println(clientAddressResponse.getReceiverAddress());
     }
